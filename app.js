dedb7a3986d52f3af7ac91a3929feb66097362f4
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-07-08 17:21:37 +0000

    commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+var pgp = require('pg-promise')({});
+var db = pgp({database: 'boditraq', user: 'postgres'});
 
 const app = express();
 
@@ -9,9 +11,33 @@ nunjucks.configure('views', {
   noCache: true
 });
 
+app.use(express.static('public'));
+
 app.get('/', (req, resp) => {
     resp.render("index.html", {});
 });
+
+app.get('/dashboard', (req, resp) => resp.redirect("/"));
+
+app.get('/track', (req,resp) => {
+    var newRecord = {
+        username: 'akw2', 
+        firstname: 'aaron', 
+        lastname: 'wilkinson', 
+        email: 'email@email.com', 
+        password_hash: '123'};
+    var insertQuery = 'INSERT INTO users VALUES (${username}, ${firstname}, ${lastname}, ${email}, ${password_hash})';
+
+    db.result(insertQuery, newRecord)
+    .then(data => console.log("SUCCESS! ", data))
+    .catch(error => console.log("ERROR: ", error));
+    
+    resp.redirect("/");
+});
+
+app.get('/account', (req, resp) => resp.redirect("/"));
+
+app.get('/logout', (req, resp) => resp.redirect("/"));
 
 app.listen(8080, function() {
     console.log("Listening on port 8080");
